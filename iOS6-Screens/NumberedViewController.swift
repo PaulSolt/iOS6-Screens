6d3d3e1061bec048764eeb815f1674d809625ab4
Paul Solt <PaulSolt@gmail.com>
2019-04-29 13:46:40 -0400

    Added a NumberViewController and a basic implementation with a centered label.

---
+++ iOS6-Screens/NumberedViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NumberedViewController.swift
+//  iOS6-Screens
+//
+//  Created by Paul Solt on 4/29/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class NumberedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 1. Customize the label
+        
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 64)
+//        label.center = view.center
+        label.text = "1"
+        label.sizeToFit()
+        label.center = view.center // Reposition after a new size
+        
+        view.addSubview(label)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 2. Figure out how to display navigation stack count
+        if let number = navigationController?.viewControllers.count {
+            label.text = String(number)
+            label.sizeToFit()
+            label.center = view.center
+        }
+    }
+    
+    let label = UILabel()  // zero sized label
+}
